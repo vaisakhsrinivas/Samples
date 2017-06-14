a2161963529378ed9db21c205f2c59d2fe2a7281
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2017-06-13 22:13:43 -0400

    New Test Odd

---
+++ Palindrome.py
@@ -0,0 +1,10 @@
+def panlindrome(s):
+    reverse = s[::-1]
+
+    if reverse == string:
+        print("Palindrome")
+    else:
+        print("Not Palindrome")
+
+string = str(input("Enter the string"))
+panlindrome(string)
