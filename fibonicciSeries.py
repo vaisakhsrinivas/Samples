7683a1ae8c0d2efc024007464236af4924a8cc0c
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2020-09-14 15:20:50 -0400

    New commits

---
+++ fibonicciSeries.py
@@ -0,0 +1,14 @@
+def fib(N):
+    res = [0,1]
+    if N == 0:
+        return res[0]
+    elif N==1:
+        return res[1]
+    for i in range(2,N+1):
+        res.append(res[-1] + res[-2])
+    return res[-1]
+
+
+
+result  = fib(4)
+print(result)
