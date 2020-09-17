6cf1c020ca868379dee6c3dd9d281c6622db4ad1
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2020-09-17 11:38:18 -0400

    New commits

---
+++ leftMostNonRepeatingChar.py
@@ -0,0 +1,12 @@
+def leftMostRepeating(s):
+    l = len(s)
+    visited = set()
+    for i in range(l):
+        if s[i] not in visited:
+            visited.add(s[i])
+            if s.count(s[i]) == 1 and s[i] in visited:
+                return i
+    return -1
+
+
+print(leftMostRepeating("aaaaabcdeffjkbllcvvde"))
