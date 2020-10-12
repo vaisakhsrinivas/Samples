7dfcfa2cee321d4b683b741b470bce7352f6870e
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2020-10-12 15:10:38 -0400

    New commits

---
+++ urlifyString.py
@@ -0,0 +1,23 @@
+def urlifyString(s):
+
+    s = s.strip()
+    l = len(s)
+    newstr = ""
+
+    for i in range (0, l):
+
+        if s[i] == " ":
+
+            newstr = newstr + '%20'
+
+        else:
+
+            newstr = newstr + s[i]
+
+    return newstr
+
+
+
+
+print(urlifyString("Cracking the coding interview    "))
+
