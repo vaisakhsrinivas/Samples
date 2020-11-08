b213c5e320ad4e804fc19f807f25f9fd068d9fd5
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2020-11-08 15:19:44 -0500

    New commits

---
+++ reverseWordsinString.py
@@ -0,0 +1,40 @@
+
+def reverse_words(s, start, end):
+
+    while start < end:
+
+        s[start], s[end] = s[end], s[start]
+
+        start = start + 1
+        end = end - 1
+
+
+s = str(input())
+
+s = list(s)
+
+start = 0
+
+
+while True:
+
+    try:
+
+        end = s.index(" ", start)
+
+        reverse_words (s, start, end-1)
+
+        start = end + 1
+
+    except ValueError:
+
+        reverse_words(s, start, len(s)-1)
+
+        break
+
+s.reverse()
+
+s = "".join(s)
+
+print (s)
+
