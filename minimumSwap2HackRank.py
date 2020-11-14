e990255ac0ad4a0fe2f3bcba12f1bcb3bb44441b
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2020-11-13 20:17:28 -0500

    New commits

---
+++ minimumSwap2HackRank.py
@@ -0,0 +1,19 @@
+def minimumSwaps(arr):
+
+    count = 0
+    for j in range(0, len(arr)):
+        while arr[j] != j+1:
+            t = arr[j]-1
+            arr[j],arr[t] = arr[t],arr[j]
+            count = count + 1
+    return count
+
+
+n = int(input())
+
+arr = list(map(int, input().rstrip().split()))
+
+res = minimumSwaps(arr)
+
+print(res)
+
