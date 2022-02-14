42461d25310136ae01d48d01029b46bed639fd74
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2022-02-14 17:00:01 -0500

    New commits

---
+++ FirstNonRepeatingInteger.py
@@ -0,0 +1,13 @@
+def find_first_unique(lst):
+    for index1 in range(len(lst)):
+        index2 = 0
+        while(index2 < len(lst)):
+            if (index1 != index2 and lst[index1] == lst[index2]):
+                break
+            index2 += 1
+        if (index2 == len(lst)):
+            return lst[index1]
+    return None
+
+
+print(find_first_unique([9, 9, 3, 2, 6, 6]))
