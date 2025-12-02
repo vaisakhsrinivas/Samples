78dfe40ce63243b296b4a9e06a1263a64f831e95
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2025-12-02 16:02:22 -0500

    modified reverseNumber

--- reverseNumber.py
+++ reverseNumber.py
@@ -1,15 +1,17 @@
 def rev(N):
 
+    if N < 0:
+        return -1 * convert(abs(N))
+    else:
+        return convert(N)
+    
+def convert(N):
     reverse = 0
-
-    while (N > 0):
-
-        a =  N % 10
+    x = N
+    while (x>0):
+        a =  x % 10
         reverse = reverse * 10 + a
-        N = N // 10
-
+        x = x // 10
     return reverse
 
-
-
-print(rev(123))
+print(rev(-123))
