484cf64200e55799c9500cb655ceed5c6d3e7607
vaisakhsrinivas <vaisakh.srinivas@gmail.com>
2026-04-24 01:45:10 -0400

    minor change

--- wordCompressor.py
+++ wordCompressor.py
@@ -1,11 +1,9 @@
 '''
 Given a string, return a compressed version of the string using the following rules:
-
 The first occurrence of a word remains unchanged.
 Subsequent occurrences are replaced with the position of the first occurrence, where the first word is at position 1.
 Words are separated by a single space.
 For example, given "practice makes perfect and perfect practice makes perfect", return "practice makes perfect and 3 1 2 3".
-
 '''
 
 def compress(s):
